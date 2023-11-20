e888b136a5efee3b35883174c5e5c135749f7d73
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-11-20 14:30:17 +0300

    .

--- Projects/react-project-1/src/Pages/Home/index.js
+++ Projects/react-project-1/src/Pages/Home/index.js
@@ -12,7 +12,9 @@ function Home() {
 
   const handleAddRandom = () => {
     var rand = Math.floor(Math.random()*99+1);
-    SetRandNumbers([...randNumbers, rand]);
+    if (!randNumbers.includes(rand)) {
+      SetRandNumbers([...randNumbers, rand]);
+    }
   }
 
   return (
