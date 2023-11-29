209c46fd9492be18aaf336147bdbd44f5a6fa865
[Elif Gümüş] <[elif.gumus@bil.omu.edu.tr]>
2023-11-29 22:42:19 +0300

    The operation of adding new products to datagrid and API has been completed.

--- Projects/react-project-1/src/Pages/DataGridSample/index.js
+++ Projects/react-project-1/src/Pages/DataGridSample/index.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { useQuery } from "react-query";
 import { DataGrid } from '@mui/x-data-grid';
-import { Container } from '@mui/material';
+import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
 
 function DataGridSample() {
   const { data, status } = useQuery("products", getProducts);
   const [rows, setRows] = useState(data);
 
-  const BASE_URL = "https://northwind.vercel.app/api/products/"
+  const BASE_URL = "https://northwind.vercel.app/api/products"
 
   async function getProducts() {
     const res = await axios.get(BASE_URL);
@@ -16,7 +16,7 @@ function DataGridSample() {
   }
 
   async function handleDelete(id) {
-    await axios.delete(BASE_URL + `${id}`).then(res => {
+    await axios.delete(BASE_URL + `/${id}`).then(res => {
       console.log("Successfully Deleted")
       const newRows = rows.filter(product => product.id != id)
       setRows(newRows)
@@ -28,58 +28,134 @@ function DataGridSample() {
       field: 'id',
       headerName: 'ID',
       type: 'number',
-      width: 90
+      width: 150,
+      headerAlign: 'center',
+      align: 'center'
     },
     {
       field: 'name',
       headerName: 'Name',
-      width: 150,
-      editable: true,
+      width: 400,
+      headerAlign: 'left',
+      align: 'left'
     },
     {
       field: 'unitPrice',
       headerName: 'Price',
       type: 'number',
-      width: 150,
-      editable: true,
+      width: 200,
+      headerAlign: 'left',
+      align: 'left'
     },
     {
       field: 'unitsInStock',
-      headerName: 'City',
-      width: 150,
-      editable: true,
+      headerName: 'Stock',
+      width: 200,
+      headerAlign: 'left',
+      align: 'left'
     },
     {
       field: 'delete',
       headerName: 'Delete',
-      sortable: false,
       width: 150,
+      headerAlign: 'center',
+      align: 'center',
+      sortable: false,
       renderCell: (params) => {
-        return <button onClick={() => { handleDelete(params.row.id) }}>Delete</button>
+        return <Button  variant="contained" color="secondary" onClick={() => { handleDelete(params.row.id) }}>Delete</Button>
       }
     }
 
   ];
+
+  const [name, SetName] = useState("")
+  const [price, SetPrice] = useState("")
+  const [stock, SetStock] = useState("")
+
+  const nameHandleChange = (e) => {
+    SetName(e.target.value)
+  }
+  const priceHandleChange = (e) => {
+    SetPrice(e.target.value)
+  }
+  const stockHandleChange = (e) => {
+    SetStock(e.target.value)
+  }
+
+  const postProduct = (product) => {
+    axios.post(BASE_URL, product).then(res => console.log("Successfully added a new product"))
+  }
+
+  const handleAdd = () => {
+    //isNaN(price) || isNaN(stock) && alert("Please give a number ") 
+    var maxId = Math.max(...rows.map(row => row.id))
+    const newObject = {
+      id: (maxId + 1),
+      name: name,
+      unitPrice: Number(price),
+      unitsInStock: Number(stock)
+    }
+    setRows([...rows, newObject])
+    postProduct(newObject)
+  }
+
 
   return (
     <>
       {status === "error" && <p>Error fetching data</p>}
       {status === "loading" && <p>Fetching data...</p>}
       {status === "success" && (
-        <Container maxWidth="lg" style={{ marginTop: "8rem" }}>
-          <DataGrid
-            rows={rows ? rows : setRows(data)}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: {
-                  pageSize: 5,
-                },
-              },
-            }}
-            pageSizeOptions={[5]}
-          />
-        </Container>
+        <Box>
+          <Container maxWidth="lg" style={{ marginTop: "8rem" }}>
+            <Grid container spacing={2} style={{ textAlign: "center", alignItems: "center" }}>
+              <Grid item xs={3}>
+                <TextField
+                  id="outlined-basic"
+                  label="Name"
+                  variant="outlined"
+                  value={name}
+                  onChange={nameHandleChange}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <TextField
+                  id="outlined-basic"
+                  label="Price"
+                  variant="outlined"
+                  value={price}
+                  onChange={priceHandleChange}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <TextField
+                  id="outlined-basic"
+                  label="Stock"
+                  variant="outlined"
+                  value={stock}
+                  onChange={stockHandleChange}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button variant="contained" color="secondary" size="large" onClick={handleAdd}>Add</Button>
+              </Grid>
+
+              <Grid item xs={12}>
+                <DataGrid
+                  rows={rows ? rows : setRows(data)}
+                  columns={columns}
+                  initialState={{
+                    pagination: {
+                      paginationModel: {
+                        pageSize: 5,
+                      },
+                    },
+                  }}
+                  pageSizeOptions={[5]}
+                />
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
       )}
     </>
   )
